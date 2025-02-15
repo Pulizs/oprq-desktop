904460bd0d8a2de2d3403afe25026bf2c8ca956c
Pedro Cruz <pedrocs.flip@gmail.com>
2025-02-15 02:06:30 -0300

    update

--- index.php
+++ index.php
@@ -2,75 +2,12 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-    <title>OPRQ</title>
+    <?php include 'views/layout/head.php'; ?>
 </head>
 
 <body>
 
-    <header>
-        <img src="img/banner.jpg" style="width: 100vw;" alt="">
-
-        <nav class="navbar navbar-expand-lg" style="background-color: #080808; padding: 5px;">
-            <div class="container-fluid">
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.html"
-                                style="color: white;">INICIO</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="subpages/objetivos.html" style="color: white;">OBJETIVOS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="subpages/fotos.html" style="color: white;">FOTOS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="subpages/jogo.html" style="color: white;">JOGO OPRQ</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="subpages/locaisDeProva.html" style="color: white;">LOCAIS DE
-                                PROVA</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false" style="color: white;">
-                                EDIÇÕES ANTERIORES
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="subpages/edicoesAnteriores.html">2022</a></li>
-                                <li><a class="dropdown-item" href="#">2023</a></li>
-                                <li><a class="dropdown-item" href="#">2024</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false" style="color: white;">
-                                PROVAS ANTERIORES
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="subpages/provasAnteriores.html">2022</a></li>
-                                <li><a class="dropdown-item" href="#">2023</a></li>
-                                <li><a class="dropdown-item" href="#">2024</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-
-                <button class="btn me-md-2" style="background-color: #2a7634; color: white;"
-                    type="button">INSCREVA-SE</button>
-            </div>
-
-        </nav>
-
-    </header>
+    <?php include 'views/layout/navbarlogged.php'; ?>
 
     <div class="bg-light p-5 rounded">
         <div class="container">
---
+++ views/layout/navbarlogged.php
@@ -0,0 +1,56 @@
+<header>
+        <img src="img/banner.jpg" style="width: 100vw;" alt="">
+
+        <nav class="navbar navbar-expand-lg" style="background-color: #080808; padding: 5px;">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php"
+                                style="color: white;">INICIO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="subpages/objetivos.html" style="color: white;">OBJETIVOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="subpages/fotos.html" style="color: white;">FOTOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="subpages/jogo.html" style="color: white;">JOGO OPRQ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="subpages/locaisDeProva.html" style="color: white;">LOCAIS DE
+                                PROVA</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false" style="color: white;">
+                                EDIÇÕES ANTERIORES
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="subpages/edicoesAnteriores.html">2022</a></li>
+                                <li><a class="dropdown-item" href="#">2023</a></li>
+                                <li><a class="dropdown-item" href="#">2024</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false" style="color: white;">
+                                PROVAS ANTERIORES
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="subpages/provasAnteriores.html">2022</a></li>
+                                <li><a class="dropdown-item" href="#">2023</a></li>
+                                <li><a class="dropdown-item" href="#">2024</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+
+                <button class="btn me-md-2" style="background-color: #2a7634; color: white;"
+                    type="button">INSCREVA-SE</button>
+            </div>
+
+        </nav>
+
+    </header>
